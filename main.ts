c816c2104aed2fab89dbf4c455a96a9ef85f954e
Superniels09-NL <67866688+Superniels09-NL@users.noreply.github.com>
2020-07-15 16:34:39 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,33 @@
 function ververs_scherm () {
     basic.clearScreen()
     tel = 0
-    for (let index = 0; index < eilanden_X_X3.length; index++) {
-        led.plot(eilanden_X_X3[tel] + X, eilanden_Y_X3[tel] + Y)
-        led.plot(eilanden_X_X3[tel] + X + 1, eilanden_Y_X3[tel] + Y)
-        led.plot(eilanden_X_X3[tel] + X - 1, eilanden_Y_X3[tel] + Y)
-        tel += 1
+    tel3 = -30
+    if (Y > -30) {
+        for (let index = 0; index < eilanden_X_X3.length; index++) {
+            led.plot(eilanden_X_X3[tel] + X, eilanden_Y_X3[tel] + Y)
+            led.plot(eilanden_X_X3[tel] + X + 1, eilanden_Y_X3[tel] + Y)
+            led.plot(eilanden_X_X3[tel] + X - 1, eilanden_Y_X3[tel] + Y)
+            tel += 1
+        }
+    } else {
+        tel4 = 40
+        for (let index = 0; index < 4; index++) {
+            for (let index = 0; index < 60; index++) {
+                led.plot(X + tel3, tel4 + Y)
+                tel3 += 1
+            }
+            tel4 += 1
+            tel3 = -30
+        }
     }
     led.plotBrightness(2, 2, 69)
 }
 gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Down, function () {
+    ververs_scherm()
+    lampje(200)
+    X += 1
+})
+input.onButtonPressed(Button.A, function () {
     ververs_scherm()
     lampje(200)
     X += 1
@@ -48,16 +66,24 @@ gamePad.onEvent(GamerBitPin.P8, GamerBitEvent.Down, function () {
 gamePad.onEvent(GamerBitPin.P1, GamerBitEvent.Down, function () {
     modus_C_en_S = "S"
 })
+input.onButtonPressed(Button.B, function () {
+    lampje(200)
+    ververs_scherm()
+    X += -1
+})
 gamePad.onEvent(GamerBitPin.P2, GamerBitEvent.Down, function () {
     modus_C_en_S = "C"
 })
 let spriengen = 0
-let modus_C_en_S = ""
+let tel4 = 0
+let tel3 = 0
 let Y = 0
 let X = 0
 let tel = 0
 let eilanden_Y_X3: number[] = []
 let eilanden_X_X3: number[] = []
+let modus_C_en_S = ""
+modus_C_en_S = "S"
 eilanden_X_X3 = [0]
 eilanden_Y_X3 = [0]
 for (let index = 0; index < randint(30, 50); index++) {
@@ -79,13 +105,6 @@ basic.forever(function () {
             Y += -1
             ververs_scherm()
             basic.pause(100)
-        }
-    }
-})
-basic.forever(function () {
-    if (modus_C_en_S == "S") {
-        if (Y < -40) {
-            Y = 40
         }
     }
 })
